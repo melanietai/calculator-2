9711cfa598559863b39101e0d1aef99d58923b7c
Caroline <carolinesitter24@gmail.com>
2022-03-16 18:02:31 -0500

    Tokenize User Input and Create Exit

--- calculator.py
+++ calculator.py
@@ -5,3 +5,36 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+"""Use 'pow' to call the power function with other tokens"""
+
+# repeat forever:
+#     read input
+#     tokenize input
+#         if the first token is "q":
+#             quit
+#         else:
+#             (decide which math function to call based on first token)
+#             if the first token is 'pow':
+#                   call the power function with the other two tokens
+
+#             (...etc.)
+
+# while exit_condition_not_reached:
+
+while True:
+    # ask user what they would like to calculate
+    user_input = (input("What would you like to calculate?: "))
+    # creating a variable token that takes the user input and creates a tokenized list
+    token_input = user_input.split(" ")
+    
+    # if user presses q, exit calculator 
+    if q in token_input:
+        print("Exit")
+        break
+    
+
+    # if user enter add, we call the add function, and repeat for other calculations
+
+#     input = consume_input()
+#     output = evaluate_input(input)
+#     print(output)
